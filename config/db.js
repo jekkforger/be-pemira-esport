96d91407eaf80d2d179826beab07cf11a9441b2b
unknown <zakimaulana0754@gmail.com>
2025-11-28 01:27:34 +0700

    update

--- config/db.js
+++ config/db.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // PENTING BANGET UNTUK RAILWAY
-  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
