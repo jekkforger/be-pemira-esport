a033bf384657074e8e8f3d1aa120360f00def6c7
jekforger <mudzaqimaulana@gmail.com>
2025-11-28 15:13:28 +0700

    update

--- config/db.js
+++ config/db.js
@@ -2,8 +2,14 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
